9208ad98ed5fda837a2ef2af44b7b0aeb0a27898
DaJon Ewing <dajone37@gmail.com>
2023-03-23 15:05:59 -0700

    refactored

--- src/Weather.js
+++ src/Weather.js
@@ -1,24 +1,22 @@
 import React from 'react';
 
+// import Card from 'react-bootstrap/Card';
 
 class Weather extends React.Component {
-  render(){
-    return(
+  render() {
+    return (
       <>
-        <h3>Daily Weather</h3>
-
-        {this.props.weatherData.map((day, indx) => {
-          return(
-            <>
-            <p>{day.date}</p>
-            <p>{day.description}</p>
-            </>
-          )
-        })}
+      {this.props.data.map((day,idx) => {
+        return (
+          <>
+          <p>{day.date}</p>
+          <p>{day.description}</p>
+          </>
+        )
+      })}
       </>
     )
   }
 }
-
 
 export default Weather;
